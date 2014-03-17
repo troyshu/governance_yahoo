ee3f6de220005d7e0afd672310c4108ddfafd81f
Troy Shu <tmshu1@gmail.com>
2014-03-17 20:46:15 +0000

    add scaffold to display governance scores from yahoo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Badgovernance::Application.routes.draw do
+  resources :governance_score_yahoos
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
